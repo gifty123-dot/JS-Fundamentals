a99a7e64cce6b3fe0bed5092be42065e160212bf
Pippah Gift <pippah124@gmail.com>
2025-05-25 15:46:16 -0700

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,6 @@
 const args = process.argv.slice(2);
-if(args.length === 0){
-console.log("No argument");
-}
-else if(args.length ===1)
-{
-console.log("Argument found");
-}
-else
-{
-console.log("Argument found");
+if (args.length === 0) {
+    console.log("No argument");
+} else {
+    console.log("Arguments found");
 }
